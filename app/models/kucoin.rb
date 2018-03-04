7afa08d76861cbcd8caadd154c6d4624d6f76e0a
Anton Smagin <smagiman@gmail.com>
2018-03-04 17:17:13 +0300

    kucoin market methods

--- app/models/kucoin.rb
+++ app/models/kucoin.rb
@@ -2,12 +2,42 @@
 class Kucoin
   HOST = 'https://api.kucoin.com'.freeze
 
+  def buy(pair, amount)
+    make_order(kucoin_pair_representation(pair), 'buy', amount)
+  end
+
+  def sell(pair, amount)
+    make_order(kucoin_pair_representation(pair), 'sell', amount)
+  end
+
+  def withdraw(coin, amount, address)
+    endpoint = "/v1/account/#{coin}/withdraw/apply"
+    payload = { amount: amount, address: address }
+    HTTParty.post(
+      "#{HOST}#{endpoint}",
+      query: payload,
+      headers: headers(endpoint, payload)
+    )
+  end
+
+  def deposit_address(coin)
+    endpoint = "/v1/account/#{coin}/wallet/address"
+    HTTParty.get(
+      "#{HOST}#{endpoint}",
+      headers: headers(endpoint)
+    )['data']['address']
+  end
+
   def prices
-    HTTParty.get('https://api.kucoin.com/v1/open/tick')
+    HTTParty.get("#{HOST}/v1/open/tick")
             .parsed_response['data']
             .map do |pair|
               [pair['symbol'].delete('-'), pair['lastDealPrice']]
             end.to_h
+  end
+
+  def price(pair, type)
+    HTTParty.get("#{HOST}/v1/#{pair}/open/tick")['data'][type]
   end
 
   def balance(coin)
@@ -17,23 +47,40 @@ class Kucoin
 
   private
 
-  def headers(endpoint, query_string = '')
+  def make_order(pair, type, amount)
+    endpoint = '/v1/order'
+    payload = { symbol: pair, type: type,
+                amount: amount, price: price(pair, type) }
+    HTTParty.post(
+      "#{HOST}#{endpoint}",
+      query: payload,
+      headers: headers(endpoint, payload)
+    )
+  end
+
+  def headers(endpoint, payload = {})
+    query_string = URI.encode_www_form(payload.sort.to_h)
     {
-     'KC-API-KEY' => api_key,
-     'KC-API-NONCE' => nonce,
-     'KC-API-SIGNATURE' => signature(endpoint)
+      'KC-API-KEY' => api_key,
+      'KC-API-NONCE' => nonce,
+      'KC-API-SIGNATURE' => signature(endpoint, query_string),
+      'Content-Type' => 'application/json'
     }
   end
 
-  def signature(endpoint, query_string = '')
+  def signature(endpoint, query_string)
     #  Arrange the parameters in ascending alphabetical order(lower cases first)
     string = "#{endpoint}/#{nonce}/#{query_string}"
-    base_64 = Base64.strict_encode64(string)
-    OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), api_secret, base_64)
+    base64 = Base64.strict_encode64(string)
+    OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), api_secret, base64)
   end
 
   def nonce
     (Time.now.to_f * 1000).to_i.to_s
+  end
+
+  def kucoin_pair_representation(pair)
+    pair[-4] == '-' ? pair : pair.insert(-4, '-')
   end
 
   def api_key
