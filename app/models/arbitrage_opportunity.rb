bbc6f7206efa68084242775931a8a6f1831108a3
Anton Smagin <smagiman@gmail.com>
2018-03-11 19:27:40 +0300

    add poloniex market

--- app/models/arbitrage_opportunity.rb
+++ app/models/arbitrage_opportunity.rb
@@ -1,20 +1,30 @@
 class ArbitrageOpportunity
   def self.call
-    new.tap do |arbitrage|
-      statistic = arbitrage.kucoin_prices.each_with_object({}) do |(pair, price), result|
-        result[pair] =
-          {
-            kucoin_buy: price[:buy],
-            kucoin_sell: price[:sell],
-            binance_buy: arbitrage.binance_prices[pair][:buy],
-            binance_sell: arbitrage.binance_prices[pair][:sell],
-            kucoin_first: ((arbitrage.binance_prices[pair][:buy] / price[:sell]) * 100.0 - 100.0).round(2),
-            binance_first: ((price[:buy] / arbitrage.binance_prices[pair][:sell]) * 100.0 - 100.0).round(2),
-          }
+    new.tap do |arb|
+      statistic = arb.symbols.each_with_object({}) do |symbol, result|
+        Arbitrage::MARKETS.each do |market|
+          result[symbol] ||= {}
+          result[symbol]["#{market}_buy"] = arb.send("#{market}_prices")[symbol].try(:[], :buy)
+          result[symbol]["#{market}_sell"] = arb.send("#{market}_prices")[symbol].try(:[], :sell)
+        end
+
+        Arbitrage::MARKETS.each_with_index do |market1|
+          (Arbitrage::MARKETS - [market1]).each do |market2|
+            if result[symbol]["#{market1}_buy"].nil? || result[symbol]["#{market2}_buy"].nil?
+              result[symbol]["#{market1}_#{market2}"] = nil
+              result[symbol]["#{market2}_#{market1}"] = nil
+            else
+              result[symbol]["#{market1}_#{market2}"] =
+                (result[symbol]["#{market2}_buy"] / result[symbol]["#{market1}_sell"] * 100.0 - 100.0).round(2)
+              result[symbol]["#{market2}_#{market1}"] =
+                 (result[symbol]["#{market1}_buy"] / result[symbol]["#{market2}_sell"] * 100.0 - 100.0).round(2)
+            end
+          end
+        end
       end
 
-      arbitrage.result = statistic.sort_by do |diff|
-        -[diff[1][:kucoin_first], diff[1][:binance_first]].max
+      arb.result = statistic.sort_by do |diff|
+        -diff[1].reject { |k, _| k['buy'] || k ['sell'] }.values.compact.max
       end.to_h
     end
   end
