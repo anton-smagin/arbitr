17ca527ed623d9f0ae64a0f04f5da88cce893a63
antonsmagin <a.smagin@key-g.com>
2018-04-06 11:18:01 +0300

    add etimated balance change change

--- app/models/bot_balance.rb
+++ app/models/bot_balance.rb
@@ -1,9 +1,19 @@
 class BotBalance < ApplicationRecord
+  class << self
+    def save_estimated
+      create(
+        livecoin: Livecoin.new.estimated_btc_balance,
+        binance: Binance.new.estimated_btc_balance
+      )
+    end
 
-  def self.save_estimated
-    create(
-      livecoin: Livecoin.new.estimated_btc_balance,
-      binance: Binance.new.estimated_btc_balance
-    )
+    def change(from:, to: Time.current)
+      first_balance =
+        where('created_at > ?', from).order(:created_at).limit(1).first
+      last_balance =
+        where('created_at < ?', to).order(created_at: :desc).limit(1).first
+        (last_balance.livecoin + last_balance.binance) /
+          (first_balance.livecoin + first_balance.binance)  * 100.0 - 100
+    end
   end
 end
