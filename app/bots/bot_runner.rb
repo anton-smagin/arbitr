3c28339ad7b2d6a0f4440b5abb567dadc43e7468
antonsmagin <a.smagin@key-g.com>
2018-04-09 13:21:41 +0300

    turn off livecoin trades

--- app/bots/bot_runner.rb
+++ app/bots/bot_runner.rb
@@ -1,8 +1,8 @@
 class BotRunner
   def self.call
     exchange = Livecoin.new
-    SpreadBot.new(exchange, 'XEMBTC', 10).run
-    SpreadBot.new(exchange, 'DASHBTC', 0.01).run
+    # SpreadBot.new(exchange, 'XEMBTC', 10).run
+    # SpreadBot.new(exchange, 'DASHBTC', 0.01).run
     SpreadBot.new(Binance.new, 'IOSTBTC', 400).run
   end
 end
