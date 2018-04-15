4f1254f13d9a50b579cf71aa890ab9c49a249504
Anton Smagin <smagiman@gmail.com>
2018-04-16 00:21:43 +0300

    add total to balance statistic

--- app/models/spread_trade.rb
+++ app/models/spread_trade.rb
@@ -4,7 +4,7 @@ class SpreadTrade < ApplicationRecord
 
   class << self
     def stats(exchange:, from:, to: Time.current)
-      stats_relation(exchange, from, to)
+      result = stats_relation(exchange, from, to)
         .group('status', 'symbol')
         .select('count(*) as count_all, status, symbol')
         .each_with_object({}) do |trade, result|
@@ -15,12 +15,28 @@ class SpreadTrade < ApplicationRecord
             result[trade.symbol][:loses] = trade.count_all
           end
         end
+      result['Total'] = total_stats(exchange, from, to)
+      result
     end
 
     def stats_relation(exchange, from, to)
       where(exchange: exchange.capitalize, status: %w[sell_failed finished])
         .where('created_at > ?', from)
         .where('updated_at < ?', to)
+    end
+
+    def total_stats(exchange, from, to)
+      stats = stats_relation(exchange, from, to)
+              .group('status')
+              .select(
+                'count(*) as count_all, ' \
+                'avg(sell_price / buy_price) * 100 - 100 as avg, status'
+              ).to_a
+      {
+        wins: stats.find { |s| s.status == 'finished' }.count_all,
+        loses: stats.find { |s| s.status == 'sell_failed' }.count_all,
+        avg_spread: (stats.reduce(0) { |sum, stat| sum + stat.avg } / 2)
+      }
     end
   end
 end
--- spec/models/spread_trade.rb
+++ spec/models/spread_trade.rb
@@ -9,9 +9,29 @@ RSpec.describe SpreadTrade, type: :model do
   let (:exchange) { 'Binance' }
 
   it '#stats' do
-    create(:spread_trade, exchange: exchange, status: 'finished', symbol: 'LTCBTC')
-    create(:spread_trade, exchange: exchange, status: 'sell_failed', symbol: 'ETHBTC')
+    create(
+      :spread_trade,
+      exchange: exchange,
+      status: 'finished',
+      symbol: 'LTCBTC',
+      buy_price: 1,
+      sell_price: 1.1
+    )
+    create(
+      :spread_trade,
+      exchange: exchange,
+      status: 'sell_failed',
+      symbol: 'ETHBTC',
+      buy_price: 1,
+      sell_price: 1.1
+    )
     expect(SpreadTrade.stats(exchange: exchange, from: 10.seconds.ago))
-      .to eq({'LTCBTC' => {wins: 1, loses: 0}, 'ETHBTC' => {wins: 0, loses: 1}})
+      .to eq(
+        {
+         'LTCBTC' => { wins: 1, loses: 0 },
+         'ETHBTC' => { wins: 0, loses: 1 },
+         'Total' => { wins: 1, loses: 1, avg_spread: 10.0 }
+         }
+      )
   end
 end
